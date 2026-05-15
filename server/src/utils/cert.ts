c03cf11a5a2058c854f82dfaa73b65f3a07723df
Shahbaz-1696 <ansarishahbaz96@gmail.com>
2026-05-15 22:39:24 +0530

    Update cert file

--- server/src/utils/cert.ts
+++ server/src/utils/cert.ts
@@ -1,5 +1,8 @@
-import { readFileSync } from 'node:fs';
-import path from 'node:path';
+// import { readFileSync } from 'node:fs';
+// import path from 'node:path';
 
-export const PRIVATE_KEY = readFileSync(path.resolve('cert/private-key.pem'));
-export const PUBLIC_KEY = readFileSync(path.resolve('cert/public-key.pub'));
+// export const PRIVATE_KEY = readFileSync(path.resolve('cert/private-key.pem'));
+// export const PUBLIC_KEY = readFileSync(path.resolve('cert/public-key.pub'));
+
+export const PRIVATE_KEY = process.env.PRIVATE_KEY!;
+export const PUBLIC_KEY = process.env.PUBLIC_KEY!;
